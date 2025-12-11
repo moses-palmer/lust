2adcd5ab97f10ad7dd8d9f5fe79e51b119ed34ff
Moses Palmér <mosespalmer@gmail.com>
2025-12-11 13:53:53 +0100

    Add script documentation

---
+++ src/doc.rs
@@ -0,0 +1,15 @@
+// Tagged values must implement a few traits
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+pub enum Tag {}
+impl crate::val::Tag for Tag {}
+
+struct Context;
+
+commands! {
+    /// # Scripting language reference
+    pub enum ScriptReference<
+        Tag = Tag,
+        Context = Context,
+    > {}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+//! # A tiny interpreter for a LISP like language
+//!
+//! This crate provides a scripting environment.
+//!
+#![cfg_attr(
+    not(feature = "doc"),
+    doc = "Please build the documentation with the feature `doc` to generate a reference.\n"
+)]
+#![cfg_attr(
+    feature = "doc",
+    doc = "Please see [here](crate::doc::ScriptReference) for a reference of the language.\n"
+)]
+
 #[macro_use]
 mod macros;
 
@@ -7,6 +20,9 @@ pub mod eval;
 pub mod ast;
 pub mod exp;
 pub mod val;
+
+#[cfg(feature = "doc")]
+pub mod doc;
 
 #[cfg(test)]
 pub(crate) mod test_helpers;
